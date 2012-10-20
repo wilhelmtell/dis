010fa59a6b6aa606ec05f1052e3ba376028bdac9
Matan Nassau <matan.nassau@gmail.com>
2012-10-20 17:19:04 -0400

    aesthetics

--- dis.sh
+++ dis.sh
@@ -2,22 +2,28 @@
 
 verify_command() {
   local cmd="$1"
-  [ -z "$cmd" ] && echo "empty command" && return 1
-  [ $cmd != "post" ] && {
+  if [ -z "$cmd" ]; then
+    echo "empty command" && return 1
+  elif [ $cmd != "post" ]; then
     echo "unrecognized command" && return 1
-  }
+  fi
   return 0
 }
 
 verify_text() {
   local text="$1"
-  [ -z "$text" ] && echo "there's an easier way to be silent" && return 1
-  [ $(echo -n "$text" |wc -c) -gt 140 ] && echo "too long a post" && return 1
+  if [ -z "$text" ]; then
+    echo "there's an easier way to be silent" && return 1
+  elif [ $(echo -n "$text" |wc -c) -gt 140 ]; then
+    echo "too long a post" && return 1
+  fi
   return 0
 }
 
 verify_argument_count() {
-  [ $# -ne 2 ] && echo "command or text not given" && return 1
+  if [ $# -ne 2 ]; then
+    echo "command or text not given" && return 1
+  fi
   return 0
 }
 
