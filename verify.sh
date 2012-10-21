00ec9cb06b24c612b8cb77105948e5853db8296f
Matan Nassau <matan.nassau@gmail.com>
2012-10-20 21:27:39 -0400

    extract verify_post_command()

--- verify.sh
+++ verify.sh
@@ -1,12 +1,20 @@
 source $SCRIPT_DIRECTORY/error.sh
 
+verify_post_command() {
+  local text="$2"
+  if [ -z "$text" ]; then
+    error "no text specified"
+  fi
+  return $?
+}
+
 verify_command() {
   local cmd="$1"
   if [ -z "$cmd" ]; then
     error "no command given"
-  elif [ $cmd = "post" -a -z "$2" ]; then
-    error "no text specified"
-  elif [ $cmd != "post" ]; then
+  elif [ $cmd = "post" ]; then
+    verify_post_command "$@"
+  else
     error "unrecognized command"
   fi
   return $?
